73aea36263b04d312c937a7c78ae7d1aba70b774
Vinicius Serpa <vinicius.serpa@gmail.com>
2018-04-29 12:21:56 -0300

    list profiles and link using django template tag

--- connectedin/profiles/urls.py
+++ connectedin/profiles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from profiles import views
 
 urlpatterns = [
-    re_path(r'^$', views.index),
-    re_path(r'^profiles/(?P<profile_id>\d+)$', views.show)
+    re_path(r'^$', views.index, name = 'index'),
+    re_path(r'^profiles/(?P<profile_id>\d+)$', views.show, name = 'show')
 ]
--- connectedin/profiles/views.py
+++ connectedin/profiles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from profiles.models import Profile
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', { 'profiles' : Profile.objects.all() })
 
 def show(request, profile_id):
     profile = Profile.objects.get(id = profile_id)
